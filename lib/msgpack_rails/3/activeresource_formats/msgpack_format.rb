835e6572b63396ce2313b05645ed48ab8c1472cb
Nathan Benes <nbenes@westinconsulting.com>
2012-02-08 17:58:47 -0700

    fix an issue with converting dates and times

--- lib/msgpack_rails/3/activeresource_formats/msgpack_format.rb
+++ lib/msgpack_rails/3/activeresource_formats/msgpack_format.rb
@@ -33,7 +33,7 @@ module ActiveResource
       end
 
       def decode_value(value)
-        if value.is_a?(String) && value[-1] == "]" && (m = value.match(/^([^\[\]]+)\[([^\[\]]+)\]$/))
+        if value.is_a?(String) && value[-1] == "]" && (m = value.match(/^([^\[\]]+)\[([^\[\]]+)\]$/)) && ['time', 'datetime', 'date'].include?(m[1].downcase)
           type = m[1]
           content = m[2]
           content.send("to_#{type.downcase}")
